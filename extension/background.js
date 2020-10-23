f7d85827fa569765c1d64fbfb2246a61e46b4915
Fivejin <37471337+Fivejin@users.noreply.github.com>
2020-10-23 17:27:53 -0400

    First upload

---
+++ extension/background.js
@@ -0,0 +1,16 @@
+var questions = [
+    {
+        prompt: "What kind of skin do you have?\n(a) Combination\n(b) Oily\n(c) Normal\n(d) Dry"
+    },
+    {
+        prompt: "How old are you?\n(a) Teens\n(b) 20+\n(c) 30+\n(d) 40+"
+    },
+    {
+        prompt: "What is your top skin concern?\n(a)Fine lines/wrinkles\n(b) Discoloration\n(c) Acne\n(d) Brightening\n(e) Dryness"
+    }
+]
+var score = 0;
+
+for (var i = 0; i < questions.length; i++) {
+    var response = window.prompt();
+}
